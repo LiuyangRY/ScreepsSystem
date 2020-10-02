fc84e9391f4177cfb0835ccdd90514ad77702065
Liuyang <1044457987@qq.com>
2020-10-02 23:07:46 +0800

    1. 新增采矿者角色。

--- TypeScripts/CreepConfig.ts
+++ TypeScripts/CreepConfig.ts
@@ -6,6 +6,7 @@ import { WallRepairer } from "./role.wallRepairer";
 import { Claimer } from "./role.claimer";
 import { LongDistanceHarvester } from "./role.longDistanceHarvester";
 import { Attacker } from "./role.attacker";
+import { Miner } from "./role.miner";
 
 // Creep 角色枚举
 export enum CreepRole {
@@ -55,7 +56,7 @@ export class CreepConfigs {
         [CreepRole.HARVESTER]: 2,
         [CreepRole.UPGRADER]: 2,
         [CreepRole.BUILDER]: 2,
-        [CreepRole.MINER]: 0,
+        [CreepRole.MINER]: 1,
         [CreepRole.CARRIER]: 0,
         [CreepRole.LONGDISTANCEHARVESTER]: 2,
         [CreepRole.REPAIRER]: 1,
@@ -67,39 +68,41 @@ export class CreepConfigs {
     // 角色定义
     static CreepRoleDefinitions: Record<CreepRole, CreepDefinition[]> = {
         [CreepRole.HARVESTER]: [
-            new CreepDefinition(CreepRole.HARVESTER, [MOVE, MOVE, MOVE, MOVE, WORK, WORK, CARRY, CARRY], 500),
-            new CreepDefinition(CreepRole.HARVESTER, [MOVE, MOVE, MOVE, WORK, CARRY], 300),
-            new CreepDefinition(CreepRole.HARVESTER, [MOVE, WORK, CARRY], 200),
+            new CreepDefinition(CreepRole.HARVESTER, [WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE], 1100),
+            new CreepDefinition(CreepRole.HARVESTER, [WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE], 800),
+            new CreepDefinition(CreepRole.HARVESTER, [WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE], 500),
+            new CreepDefinition(CreepRole.HARVESTER, [WORK, CARRY, MOVE, MOVE, MOVE], 300),
+            new CreepDefinition(CreepRole.HARVESTER, [WORK, CARRY, MOVE], 200)
           ],
           [CreepRole.UPGRADER]: [
-            new CreepDefinition(CreepRole.UPGRADER, [WORK, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY, CARRY], 550),
+            new CreepDefinition(CreepRole.UPGRADER, [WORK, WORK, WORK, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY, CARRY], 650),
             new CreepDefinition(CreepRole.UPGRADER, [WORK, MOVE, CARRY, MOVE, MOVE], 300),
-            new CreepDefinition(CreepRole.UPGRADER, [WORK, CARRY, MOVE], 200),
+            new CreepDefinition(CreepRole.UPGRADER, [WORK, CARRY, MOVE], 200)
           ],
           [CreepRole.BUILDER]: [
             new CreepDefinition(CreepRole.BUILDER, [WORK, WORK, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY], 550),
-            new CreepDefinition(CreepRole.BUILDER, [WORK, MOVE, MOVE, CARRY, CARRY], 300),
+            new CreepDefinition(CreepRole.BUILDER, [WORK, MOVE, MOVE, CARRY, CARRY], 300)
           ],
           [CreepRole.LONGDISTANCEHARVESTER]: [
-            new CreepDefinition(CreepRole.LONGDISTANCEHARVESTER, [WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE], 500),
+            new CreepDefinition(CreepRole.LONGDISTANCEHARVESTER, [WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE], 500)
           ],
           [CreepRole.REPAIRER]: [
-            new CreepDefinition(CreepRole.REPAIRER, [WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE], 500),
+            new CreepDefinition(CreepRole.REPAIRER, [WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE], 500)
           ],
           [CreepRole.WALLREPAIRER]: [
-            new CreepDefinition(CreepRole.WALLREPAIRER, [WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE], 500),
+            new CreepDefinition(CreepRole.WALLREPAIRER, [WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE], 500)
           ],
           [CreepRole.ATTACKER]: [
-            new CreepDefinition(CreepRole.ATTACKER, [ATTACK, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE], 380),
+            new CreepDefinition(CreepRole.ATTACKER, [ATTACK, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE], 380)
           ],
           [CreepRole.CLAIMER]: [
-            new CreepDefinition(CreepRole.CLAIMER, [CLAIM, MOVE], 650),
+            new CreepDefinition(CreepRole.CLAIMER, [CLAIM, MOVE], 650)
           ],
           [CreepRole.MINER]: [
-            new CreepDefinition(CreepRole.MINER, [WORK, WORK, WORK, WORK, WORK, MOVE], 550),
+            new CreepDefinition(CreepRole.MINER, [WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE], 800)
           ],
           [CreepRole.CARRIER]: [
-            new CreepDefinition(CreepRole.CARRIER, [CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE], 500),
+            new CreepDefinition(CreepRole.CARRIER, [CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE], 500)
           ]
     };
 
@@ -112,37 +115,36 @@ export class CreepConfigs {
     Work(creep: Creep){
         let working: boolean = false;
         let worker: Harvester | Builder | Upgrader | Repairer | Claimer | null = null;
-        switch (creep.memory["role"]) {
-            case "harvester":
-            case "harvesterV2":
-                worker = new Harvester();
-                break;
-            
-            case "longDistanceHarvester":
-                worker = new LongDistanceHarvester();
-                break;
-            case "builder":
-            case "builderV2":
-                worker = new Builder();
-                break;
-            case "upgrader":
-            case "upgraderV2":
-                worker = new Upgrader();
-                break;
-            case "repairer":
-            case "repairerV2":
-                worker = new Repairer();
-                break;
-            case "wallRepairer":
-            case "wallRepairerV2":
-                worker = new WallRepairer();
-                break;
-            case "claimer":
-                worker = new Claimer();
-                break;
-            case "attacker":
-                worker = new Attacker();
-                break;
+        switch (creep.memory["role"] as CreepRole) {
+            case CreepRole.HARVESTER:
+              worker = new Harvester();
+              break;
+            case CreepRole.LONGDISTANCEHARVESTER:
+              worker = new LongDistanceHarvester();
+              break;
+            case CreepRole.BUILDER:
+              worker = new Builder();
+              break;
+            case CreepRole.UPGRADER:
+              worker = new Upgrader();
+              break;
+            case CreepRole.REPAIRER:
+              worker = new Repairer();
+              break;
+            case CreepRole.WALLREPAIRER:
+              worker = new WallRepairer();
+              break;
+            case CreepRole.CLAIMER:
+              worker = new Claimer();
+              break;
+            case CreepRole.ATTACKER:
+              worker = new Attacker();
+              break;
+            case CreepRole.MINER:
+              worker = new Miner();
+              break;
+            case CreepRole.CARRIER:
+              break;
         }
         working = worker?.Switch ? worker.Switch(creep) : true;
         if(!!worker){
---
+++ TypeScripts/role.miner.ts
@@ -0,0 +1,92 @@
+import { FindClostestStorageForStoring, IsFull, ObtainTakeMethod } from "./CreepCommonMethod";
+import { ICreepConfig } from "./ICreepConfig"
+
+export class Miner implements ICreepConfig{
+
+    /**
+     * Miner 类的构造函数
+     * @property color creep 路径的颜色
+     */
+    constructor(color: string = "#1d2027") {
+        this.pathColor = color;
+    }
+
+    // 路径颜色
+    pathColor: string;
+
+    // 采矿
+    Source(creep: Creep): any {
+        if(!!!creep.memory.source){
+            // 寻找最近的矿场
+            const mineral: Mineral | null = creep.pos.findClosestByRange(FIND_MINERALS,{
+                filter: (m) => (
+                    m.mineralAmount > 0 || m.ticksToRegeneration < 100
+                )
+            });
+            if(!!mineral){
+                creep.memory.source = mineral.id;
+                creep.memory.energyTakeMethod = "harvest";
+            }else{
+                console.log(`Creep: ${creep.name} 的采集目标不存在。`)
+                return;
+            }
+        }else{
+            const mineral = Game.getObjectById(creep.memory.source as Id<Mineral>);
+            if(!!mineral){
+                const result = creep.harvest(mineral);
+                if (result == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(mineral, { visualizePathStyle: { stroke: this.pathColor }});
+                }
+                if(result == ERR_NOT_ENOUGH_RESOURCES) {
+                    creep.memory.source = undefined;
+                }
+            }
+        }
+    }
+
+    // 存储能量
+    Target(creep: Creep): any {
+        if(creep.room.energyAvailable < creep.room.energyCapacityAvailable){
+            creep.memory.storage = undefined;
+        }
+        if (!!!creep.memory.storage) {
+            const assignedId = FindClostestStorageForStoring(creep);
+            if (!!!assignedId) {
+                return;
+            }else{
+                creep.memory.storage = assignedId.id;
+            }
+        }
+        const assignedStorage = Game.getObjectById(creep.memory.storage as Id<StructureLink | StructureContainer>);
+        if(!!assignedStorage){
+            let sourceId = assignedStorage.id;
+            let methodType = ObtainTakeMethod(assignedStorage);
+            if(!IsFull({ id: sourceId, take: methodType})){
+                if(creep.transfer(assignedStorage, RESOURCE_ZYNTHIUM) == ERR_NOT_IN_RANGE){
+                    creep.moveTo(assignedStorage, { visualizePathStyle: { stroke: this.pathColor }});
+                }
+            }else{
+                creep.memory.storage = undefined;
+            }
+        }else{
+            creep.memory.storage = undefined;
+        }
+    }
+
+    Switch(creep: Creep): boolean  {
+        // 切换工作状态
+        // creep 身上没有能量且 creep 之前的工作状态为“工作”
+        if(creep.store[RESOURCE_ZYNTHIUM] <= 0 && !!creep.memory.working){
+            creep.memory.working = false;
+            delete creep.memory.storage;
+            creep.say("🔄 执行采集工作。");
+        }
+        // creep 身上能量已满且 creep 之前的工作状态为“不工作”
+        if(creep.store[RESOURCE_ZYNTHIUM] >= creep.store.getCapacity() && !!!creep.memory.working){
+            creep.memory.working = true;
+            creep.say("🚧 执行存储工作。");
+        }
+        return creep.memory.working;
+    }
+    
+}
